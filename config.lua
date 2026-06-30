d7609d3dffb1e612cb285c3d1c329c6aa16140c3
Fluid <d8p3v9b0g3@gmail.com>
2026-06-30 19:17:27 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,5 @@
+fd.disablecrosshair = true -- Fully disables the default GTA 5 crosshair globally
+fd.firstpersononfoot = false -- Force first person view when aiming on foot
+fd.firstpersoninvehicle = true -- Force first person view when aiming from a vehicle
+fd.disablebikekick = true -- Disable melee kick while on bikes
+fd.disablesilentkill = true -- Disable stealth takedown kills
